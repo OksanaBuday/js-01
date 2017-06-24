89b9fc4c1173e7bcb3ffecb455d7e557b87a4eee
Oksana Buday <oksanabuday@gmail.com>
2017-06-24 20:25:29 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-// alert ('Hello World');
-// document.wrire('Hello World');
-// console.log('Hello World');
+// // alert ('Hello World');
+// // document.wrire('Hello World');
+// // console.log('Hello World');
 
-var userName = prompt('Enter yor name');
+// var userName = prompt('Enter your name');
+// document.write('Hello ' +  userName);
+// var userAge =  Number (prompt('Enter your age'));
+// document.write('<br>')
+// document.write(' age is ' + userAge);
+// var newAge = sumThree(userAge,1,1); //userAge +1;
+// document.write('<br>')
+// document.write(' After the next year you will be' + newAge);
+// document.write('<br>');
+// document.write('You are ' + checkAge(userAge));
+
+// function sum(a,b){
+//     var result= a + b;
+//     return result;
+// }
+
+// function sumThree (a,b,c){
+//     var result = a + b +c;
+//     return result;
+// }
+
+// function checkAge(age){
+//     if (age < 35){
+//         return 'young'
+//     }else {return 'not young'
+        
+//     }
+// }
+
+document.write( Math.sqrt(4));
